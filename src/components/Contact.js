529cb94d04e4de515b7ed6d1f767ecc0e9f39ae7
SandyNabil <sandynabil257@gmail.com>
2024-07-04 17:30:16 +0300

    Update Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,123 +1,123 @@
-    import React from "react";
-    import {
-    Container,
-    Typography,
-    Grid,
-    Box,
-    IconButton,
-    useMediaQuery,
-    useTheme,
-    } from "@mui/material";
-    import FacebookIcon from "@mui/icons-material/Facebook";
-    import EmailIcon from "@mui/icons-material/Email";
-    import PhoneIcon from "@mui/icons-material/Phone";
+        import React from "react";
+        import {
+        Container,
+        Typography,
+        Grid,
+        Box,
+        IconButton,
+        useMediaQuery,
+        useTheme,
+        } from "@mui/material";
+        import FacebookIcon from "@mui/icons-material/Facebook";
+        import EmailIcon from "@mui/icons-material/Email";
+        import PhoneIcon from "@mui/icons-material/Phone";
 
-    const Contact = () => {
-    const theme = useTheme();
-    const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
+        const Contact = () => {
+        const theme = useTheme();
+        const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
 
-    return (
+        return (
         <div
-        id="Contact"
-        style={{
+            id="Contact"
+            style={{
             padding: "20px 0",
             backgroundColor: "#194662",
             color: "#fff",
             minHeight: "20vh",
-        }}
+            }}
         >
-        <Container>
+            <Container>
             {/* Section Title */}
             <Typography
-            variant="h2"
-            sx={{
+                variant="h2"
+                sx={{
                 marginBottom: "20px",
                 textAlign: "center",
                 fontWeight: "bold",
-            }}
+                }}
             >
-            Contact Us
+                Contact Us
             </Typography>
 
             {/* Grid for Contact Details */}
             <Grid
-            container
-            direction="column"
-            alignItems={isMobile ? "flex-start" : "center"}
-            spacing={2}
+                container
+                direction="column"
+                alignItems={isMobile ? "flex-start" : "center"}
+                spacing={2}
             >
-            {/* Gmail */}
-            <Grid item>
+                {/* Gmail */}
+                <Grid item>
                 <Box display="flex" alignItems="center">
-                <IconButton
+                    <IconButton
                     href="mailto:Abanoubgirgisabanoub@gmail.com"
                     sx={{ color: "#fff" }}
-                >
+                    >
                     <EmailIcon sx={{ fontSize: 45, color: "red" }} />
-                </IconButton>
-                            <Typography
-                                variant="h3"
+                    </IconButton>
+                    <Typography
+                    variant="h3"
                     sx={{
-                    fontFamily: "Arial",
-                    marginLeft: isMobile ? "7px" : "15px",
-                    fontSize: isMobile ? "1rem" : "1.5rem",
-                    color: "#BBBBBB",
-                    textShadow: "none",
+                        fontFamily: "Arial",
+                        marginLeft: isMobile ? "7px" : "15px",
+                        fontSize: isMobile ? "1rem" : "1.5rem",
+                        color: "#BBBBBB",
+                        textShadow: "none",
                     }}
-                >
-                    Abanoubgirgisabanoub@gmail.com
-                </Typography>
+                    >
+                    abanoubgirgiabanoub@gmail.com
+                    </Typography>
                 </Box>
-            </Grid>
+                </Grid>
 
-            {/* Phone */}
-            <Grid item>
+                {/* Phone */}
+                <Grid item>
                 <Box display="flex" alignItems="center">
-                <IconButton sx={{ color: "#fff" }}>
+                    <IconButton sx={{ color: "#fff" }}>
                     <PhoneIcon sx={{ fontSize: 50, color: "green" }} />
-                </IconButton>
-                <Typography
+                    </IconButton>
+                    <Typography
                     variant="body1"
                     sx={{
-                    marginLeft: isMobile ? "7px" : "15px",
-                    fontSize: isMobile ? "1.5rem" : "2rem",
-                    color: "#BBBBBB",
-                    textShadow: "none",
+                        marginLeft: isMobile ? "7px" : "15px",
+                        fontSize: isMobile ? "1.5rem" : "2rem",
+                        color: "#BBBBBB",
+                        textShadow: "none",
                     }}
-                >
+                    >
                     +20 128 699 7506
-                </Typography>
+                    </Typography>
                 </Box>
-            </Grid>
+                </Grid>
 
-            {/* Facebook */}
-            <Grid item>
+                {/* Facebook */}
+                <Grid item>
                 <Box display="flex" alignItems="center">
-                <IconButton
+                    <IconButton
                     href="https://www.facebook.com/abanoub.girgis.56"
                     target="_blank"
                     rel="noopener noreferrer"
                     sx={{ color: "#fff" }}
-                >
+                    >
                     <FacebookIcon sx={{ fontSize: 50, color: "#4267B2" }} />
-                </IconButton>
-                <Typography
+                    </IconButton>
+                    <Typography
                     variant="body1"
                     sx={{
-                    marginLeft: isMobile ? "7px" : "15px",
-                    fontSize: isMobile ? "1.5rem" : "2rem",
-                    color: "#BBBBBB",
-                    textShadow: "none",
+                        marginLeft: isMobile ? "7px" : "15px",
+                        fontSize: isMobile ? "1.5rem" : "2rem",
+                        color: "#BBBBBB",
+                        textShadow: "none",
                     }}
-                >
+                    >
                     Abanoub Girgis
-                </Typography>
+                    </Typography>
                 </Box>
+                </Grid>
             </Grid>
-            </Grid>
-        </Container>
+            </Container>
         </div>
-    );
-    };
+        );
+        };
 
-    export default Contact;
+        export default Contact;
